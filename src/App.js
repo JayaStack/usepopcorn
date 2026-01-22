115887ad19bf5d4016995ffc674030a1c5de5861
Your Name <your@email.com>
2026-01-22 22:09:30 +0530

    Cleaning Up the Title

--- src/App.js
+++ src/App.js
@@ -323,6 +323,10 @@ function MovieDetails({ SelectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title],
   );
